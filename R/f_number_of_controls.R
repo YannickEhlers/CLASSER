8f281af5a30dfe5d6b76db1d2c8ba39917884371
ar0it <60975243+ar0it@users.noreply.github.com>
2020-05-27 16:19:42 +0200

    Update f_number_of_controls.R

--- R/f_number_of_controls.R
+++ R/f_number_of_controls.R
@@ -1,5 +1,5 @@
 f_number_of_controls <- function(v_cases, v_prev){
-  v_controls = v_cases*((1-v_prev)/prev)
+  v_controls = v_cases*((1-v_prev)/v_prev)
   return(list("Benötigte Kontrollen" = v_controls))
 
 }
