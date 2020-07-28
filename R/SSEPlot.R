f637c3459e1edac286778d5e581a6262ba22d244
YannickEhlers <51520476+YannickEhlers@users.noreply.github.com>
2020-07-28 19:39:28 +0200

    Update SSEPlot.R

--- R/SSEPlot.R
+++ R/SSEPlot.R
@@ -1,22 +1,18 @@
-SSEPlot <- function(SimulatedSSEObject){
+SSEPlot <- function(SSEObject){
   
-  SampleSizeWithBootstraps <- cbind(SSE@SampleSizesTested, SSE@MeasuredMatrix[,1], SSE@MeasuredMatrix[,2],SSE@MeasuredMatrix[,3],SSE@MeasuredMatrix[,4],SSE@MeasuredMatrix[,5],SSE@MeasuredMatrix[,6],SSE@MeasuredMatrix[,7],SSE@MeasuredMatrix[,8],SSE@MeasuredMatrix[,9],SSE@MeasuredMatrix[,10])
-  
-  loessRegression <- loess(rowMeans(SSE@MeasuredMatrix) ~ SSE@SampleSizesTested)
-  
-  plot(SampleSizeWithBootstrapsDataframe$V1, SampleSizeWithBootstrapsDataframe$V2, ylim = c(-0.1, 1.1))
-  points(SampleSizeWithBootstrapsDataframe$V1, SampleSizeWithBootstrapsDataframe$V3)
-  points(SampleSizeWithBootstrapsDataframe$V1, SampleSizeWithBootstrapsDataframe$V4)
-  points(SampleSizeWithBootstrapsDataframe$V1, SampleSizeWithBootstrapsDataframe$V5)
-  points(SampleSizeWithBootstrapsDataframe$V1, SampleSizeWithBootstrapsDataframe$V6)
-  points(SampleSizeWithBootstrapsDataframe$V1, SampleSizeWithBootstrapsDataframe$V7)
-  points(SampleSizeWithBootstrapsDataframe$V1, SampleSizeWithBootstrapsDataframe$V8)
-  points(SampleSizeWithBootstrapsDataframe$V1, SampleSizeWithBootstrapsDataframe$V9)
-  points(SampleSizeWithBootstrapsDataframe$V1, SampleSizeWithBootstrapsDataframe$V10)
-  
+  loessRegression <- loess(rowMeans(SSEObject@MeasuredMatrix) ~ SSEObject@SampleSizesTested)
+
   PreductionLoess <- predict(loessRegression)
   
-  lines(PreductionLoess)
-  lines(PreductionLoess + 2*apply(SSE@MeasuredMatrix, 1, sd))
-  lines(PreductionLoess - 2*apply(SSE@MeasuredMatrix, 1, sd))
-}
+  plot(PreductionLoess, type = "l", ylim = c(0,1), SimulatedSSEObject)
+  
+  
+  PredictionUpperBorder <- PreductionLoess + 2*apply(SSEObject@MeasuredMatrix, 1, sd)
+  PredictionUpperBorder[which(PredictionUpperBorder > 1)] <- 1
+
+  PredictionLowerBorder <- PreductionLoess - 2*apply(SSEObject@MeasuredMatrix, 1, sd)
+  PredictionLowerBorder[which(PredictionLowerBorder < 0)] <- 0
+  
+  lines(predict(loess(PredictionLowerBorder ~ SSEObject@SampleSizesTested)), lty = 2)
+  lines(predict(loess(PredictionUpperBorder ~ SSEObject@SampleSizesTested)), lty = 2)
+  }
